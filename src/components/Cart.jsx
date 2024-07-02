e4f295183cfefb045209eb320cdf9be12d8dbca7
= <=>
2024-07-02 12:30:04 -0400

    Fixed minor bugs + navbar working with cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,12 @@
 "use client";
-import { useState, useEffect } from "react";
-import NavBar from "@/components/Inventory";
+import { useEffect, useState } from "react";
+import { BsFillBagCheckFill } from "react-icons/bs";
+import { FaMinus, FaPlus } from "react-icons/fa6";
+import { IoIosClose } from "react-icons/io";
+import React from "react";
+import "../styles/global.css";
 
-function Cart({ Component, pageProps }) {
+const Cart = React.forwardRef(function Cart({ toggleCart }, ref) {
   const [cart, setCart] = useState({});
   const [subTotal, setSubTotal] = useState(0);
   useEffect(() => {
@@ -15,7 +19,7 @@ function Cart({ Component, pageProps }) {
       console.error(error);
       localStorage.clear();
     }
-  });
+  }, [setCart]);
 
   const saveCart = () => {
     localStorage.setItem("cart", myCart);
@@ -57,13 +61,40 @@ function Cart({ Component, pageProps }) {
   };
 
   return (
-    <NavBar
-      cart={cart}
-      addToCart={addToCart}
-      removeFromCart={removeFromCart}
-      clearCart={clearCart}
-      subTotal={subTotal}
-    />
+    <div
+      ref={ref}
+      className="w-72 sideCart  absolute top-0 right-0 bg-purple-400 px-4 py-10 transform transition-transform translate-x-full"
+    >
+      <h2 className="font-bold text-xm text-center">Shopping Cart</h2>
+      <span
+        onClick={toggleCart}
+        className="absolute top-1 right-0 cursor-pointer text-2xl text-purple-600"
+      >
+        <IoIosClose />
+      </span>
+      <ol className="list-decimal font-semibold">
+        <li>
+          <div className="item flex my-5">
+            <div className="w-2/3 font-semibold">1.Product apple</div>
+            <div className="flex font-semibold items-center justify-center w-1/3">
+              <FaMinus className="mx-3 cursor-pointer" />1
+              <FaPlus className="cursor-pointer mx-3" />
+            </div>
+          </div>
+        </li>
+      </ol>
+      <div className="flex">
+        <button className="flex mr-2  text-white bg-pink-500 , border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm">
+          <BsFillBagCheckFill className="m-1" />
+          Checkout
+        </button>
+
+        <button className="flex mr-2  text-white bg-pink-500 , border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm">
+          <BsFillBagCheckFill className="m-1" />
+          Clear Cart
+        </button>
+      </div>
+    </div>
   );
-}
+});
 export default Cart;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,26 +1,26 @@
 export default function ProductCard({ name, price, description, img_url }) {
   return (
-    <div class="flex flex-col justify-around w-[300px] h-[500px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="flex flex-col justify-around w-[300px] h-[500px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-        <img class="rounded-t-lg min-w-full" src={img_url} alt="${name}" />
+        <img className="rounded-t-lg min-w-full" src={img_url} alt="${name}" />
       </a>
-      <div class="p-5 grow flex flex-col">
+      <div className="p-5 grow flex flex-col">
         <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {name}
           </h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {description}
         </p>
-        <div class="flex items-center justify-between mt-auto">
+        <div className="flex items-center justify-between mt-auto">
           <a
             href="#"
-            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Add to Cart
             <svg
-              class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -28,14 +28,14 @@ export default function ProductCard({ name, price, description, img_url }) {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
           </a>
-          <span class="text-2xl font-bold text-gray-900 dark:text-white">
+          <span className="text-2xl font-bold text-gray-900 dark:text-white">
             ${price}
           </span>
         </div>
