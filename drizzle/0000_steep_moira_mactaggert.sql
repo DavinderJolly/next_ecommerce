46b4b46b67e004f91747f70caf04406000d6735b
DavinderJolly <davinderrocks0786@gmail.com>
2025-07-10 14:32:54 +0530

    update build command to use drizzle instead of prisma

---
+++ drizzle/0000_steep_moira_mactaggert.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "products" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "products_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"name" varchar(255) NOT NULL,
+	"description" varchar(1000) NOT NULL,
+	"img_url" varchar(1000) NOT NULL,
+	"price" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"name" varchar(255) NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
